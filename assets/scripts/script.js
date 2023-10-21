5ff4f4e731fa64cc2885ee73f1dfbbaf6b7edc98
MazurkiewiczR <126096732+MazurkiewiczR@users.noreply.github.com>
2023-10-21 17:56:07 +0200

    started creating mobile menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const hamburgerMenu = document.querySelector(".hamburger");
 
   hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
