2133f0a0f7904c4b41a82d410ed39ef8f9b14ef2
MazurkiewiczR <126096732+MazurkiewiczR@users.noreply.github.com>
2023-10-22 13:25:10 +0200

    created responsive nav

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.querySelector(".hamburger");
+  const mobileMenu = document.querySelector(".mobile-nav");
 
   hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
   });
 });
