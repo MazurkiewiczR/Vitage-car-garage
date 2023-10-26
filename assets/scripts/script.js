cea2f618d0560f5a729031de7cfe215d2e41ab46
MazurkiewiczR <mazurkiewicz.er@gmail.com>
2023-10-26 19:41:50 +0200

    parallax function created
    and finished services section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,21 +12,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //
 
-document.addEventListener("DOMContentLoaded", function () {
-  const imageParallax = document.querySelector(".image-parallax");
-  const imageContainer = document.querySelector(".image-container");
-  const imageText = document.querySelector(".image-text");
+const parallaxScroll = (parallaxClass, scrollSpeed) => {
+  const imageParallax = document.querySelector(`.${parallaxClass}`);
 
   window.addEventListener("scroll", function () {
     const scrollValue = window.scrollY;
-    const translateY = -scrollValue * 0.4;
+    const translateY = -scrollValue * scrollSpeed;
 
     imageParallax.style.transform = `translateY(${translateY}px)`;
     const containerHeight = window.getComputedStyle(imageParallax).height;
     imageParallax.style.height =
       Number(containerHeight.slice(0, -2)) - translateY + "px";
-    if (imageParallax.style.height > "10000px") {
+    if (imageParallax.style.height > "5000px") {
       imageParallax.style.height = "4000px";
     }
   });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth < 768) {
+    return;
+  }
+
+  parallaxScroll("parallax", 0.1);
 });
