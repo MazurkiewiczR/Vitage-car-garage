406b733680a55ba90e9c0ba071d7de29549b3ffe
MazurkiewiczR <mazurkiewicz.er@gmail.com>
2023-10-26 19:03:43 +0200

    parallax section adedd

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,5 +5,28 @@ document.addEventListener("DOMContentLoaded", function () {
   hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+
+    document.body.classList.toggle("no-scroll");
+  });
+});
+
+//
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imageParallax = document.querySelector(".image-parallax");
+  const imageContainer = document.querySelector(".image-container");
+  const imageText = document.querySelector(".image-text");
+
+  window.addEventListener("scroll", function () {
+    const scrollValue = window.scrollY;
+    const translateY = -scrollValue * 0.4;
+
+    imageParallax.style.transform = `translateY(${translateY}px)`;
+    const containerHeight = window.getComputedStyle(imageParallax).height;
+    imageParallax.style.height =
+      Number(containerHeight.slice(0, -2)) - translateY + "px";
+    if (imageParallax.style.height > "10000px") {
+      imageParallax.style.height = "4000px";
+    }
   });
 });
