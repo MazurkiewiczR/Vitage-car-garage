198d49b1c4f771e1a06113401a17a73a11a1f945
MazurkiewiczR <126096732+MazurkiewiczR@users.noreply.github.com>
2023-11-08 22:29:30 +0100

    tried another parallax method

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,28 +11,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //
-
-const parallaxScroll = (parallaxClass, scrollSpeed) => {
-  const imageParallax = document.querySelector(`.${parallaxClass}`);
-
-  window.addEventListener("scroll", function () {
-    const scrollValue = window.scrollY;
-    const translateY = -scrollValue * scrollSpeed;
-
-    imageParallax.style.transform = `translateY(${translateY}px)`;
-    const containerHeight = window.getComputedStyle(imageParallax).height;
-    imageParallax.style.height =
-      Number(containerHeight.slice(0, -2)) - translateY + "px";
-    if (imageParallax.style.height > "5000px") {
-      imageParallax.style.height = "700px";
-    }
-  });
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.innerWidth < 768) {
-    return;
-  }
-
-  parallaxScroll("parallax-bg", -0.2);
-});
