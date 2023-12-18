e8ca13dfc786c7227e470c8ebff18c3e75cfd59c
MazurkiewiczR <mazurkiewicz.er@gmail.com>
2023-12-18 10:39:42 +0100

    added lightbox to the gallery section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -82,3 +82,61 @@ blurredSections.forEach((section) => {
     img.addEventListener("load", loaded);
   }
 });
+
+//GALLERY
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+function hideMenu() {
+  const desktopMenu = document.querySelector(".desktop-nav");
+
+  desktopMenu.classList.add("hidden");
+}
+
+function showMenu() {
+  const desktopMenu = document.querySelector(".desktop-nav");
+
+  if (!desktopMenu.classList.contains("hidden")) {
+    return;
+  } else {
+    desktopMenu.classList.remove("hidden");
+  }
+}
